a4e87552713ac7c820063fe38057781d63b2f533
nakaikento <okamoto2okamoto@gmail.com>
2019-07-20 21:08:34 +0900

    added user update page

--- your_shelf/accounts/urls.py
+++ your_shelf/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'accounts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.SignupView.as_view(), name='signup'),
+    path('update/<int:pk>/', views.AccountUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', views.AccountDelete.as_view(), name='delete'),
     path('<str:user_name>/', views.user_detail, name='detail'),
 ]
--- your_shelf/accounts/views.py
+++ your_shelf/accounts/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render
 from .models import CustomUser
 from books.models import Book
 from .forms import SignupForm
-
+from django.views.generic.edit import UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
 
 class SignupView(CreateView):
     form_class = SignupForm
@@ -32,3 +33,19 @@ def user_detail(request, user_name):
         'borrowing_books': [borrowing_book for borrowing_book in borrowing_books],
     }
     return render(request, 'accounts/account_detail.html', context)
+
+
+class AccountUpdate(UpdateView):
+    model = CustomUser
+    template_name = 'accounts/account_update.html'
+    success_url = reverse_lazy('accounts:index')
+    fields = ['username',  'photo', 'email', 'interest']
+    tempalte_name_suffix = '_update_form'
+
+class AccountDelete(DeleteView):
+    model = CustomUser
+    template_name = 'accounts/account_delete.html'
+    success_url = reverse_lazy('accounts:index')
+
+# account_update = login_required(redirect_field_name='redirect_to')(AccountUpdate.as_view())
+# account_delete = login_required(redirect_field_name='redirect_to')(AccountDelete.as_view())
